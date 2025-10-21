579e81dc9f2894198c93eb22ea933eb7abec2ab8
João Alessandro <joao.jo.al@outlook.com>
2025-10-21 19:32:23 -0300

    testes12

--- Prova_Algoritmo/2/js/script.js
+++ Prova_Algoritmo/2/js/script.js
@@ -1,4 +1,4 @@
-function calcularDivisao() {
+function calcularquociente() {
     let inNumero1 = Number(document.getElementById("inNumero1").value);
     let inNumero2 = Number(document.getElementById("inNumero2").value);
     
@@ -6,19 +6,38 @@ function calcularDivisao() {
         alert("Não é possível dividir por zero!");
         return;
     }
-    
-    let outResposta = document.getElementById("outResposta");
     let quociente = 0;
+    let dividendo = inNumero1;
+    let divisor = inNumero2;
+    while (dividendo >= divisor) {
+        dividendo = dividendo - divisor;
+        quociente++;
+    }
+     return "Quociente: " + quociente; 
+}
+
+function calcularResto() {
+    let inNumero1 = Number(document.getElementById("inNumero1").value);
+    let inNumero2 = Number(document.getElementById("inNumero2").value);
+    if (inNumero2 === 0) {
+        alert("Não é possível dividir por zero!");
+        return;  
+    }
     let resto = 0;
-    
     let dividendo = inNumero1;
     let divisor = inNumero2;
 
     while (dividendo >= divisor) {
         dividendo = dividendo - divisor;
-        quociente++;
     }
     resto = dividendo;
+    return 'Resto:' + resto+'\n'; 
+}
 
-    outResposta.innerText = "Quociente: " + quociente + " Resto: " + resto; 
+function onButtonClick() {
+    let resposta = '\n';
+    let outResposta = document.getElementById("outResposta");
+    resposta += calcularResto();
+    resposta += calcularquociente();
+    outResposta.innerText = resposta;
 }
