0a5abf9626c3e9d28567a5fd133b8659f8edf3c7
João Alessandro <joao.jo.al@outlook.com>
2025-11-13 20:48:46 -0300

    qiso

--- AtividadeGustavo/js/script.js
+++ AtividadeGustavo/js/script.js
@@ -3,18 +3,44 @@ function tocarSom() {
     let count = parseInt(contador.value);
     count *= -1;
 
-    //array de todos os audios da pagina para parar
-    const audio = document.getElementById("audioCaneta");
     const audios = document.querySelectorAll("audio");
-
     audios.forEach(a => {
         a.pause();
         a.currentTime = 0;
     });
+
     if (count === 1) {
-        if (audio) {
-            audio.play();
-        }
+        const audio = document.getElementById("audioCaneta");
+        if (audio) audio.play();
     }
+
     contador.value = count;
 }
+
+function incrementaNum() {
+    let label = document.getElementById("contadorLabel");
+    let atual = parseInt(label.textContent);
+    label.textContent = atual + 1;
+}
+
+function diminuiNum() {
+    let label = document.getElementById("contadorLabel");
+    let atual = parseInt(label.textContent);
+    label.textContent = atual - 1;
+}
+
+let botao = document.getElementById("btnContador");
+
+botao.addEventListener('mousedown', function (e) {
+    e.preventDefault();
+
+    if (e.button === 0) {
+        incrementaNum();
+    } else if (e.button === 2) {
+        diminuiNum();
+    }
+});
+
+botao.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+});
