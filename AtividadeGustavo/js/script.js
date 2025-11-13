27ad37932627199f491bf988ab2019dd1019d5e3
João Alessandro <joao.jo.al@outlook.com>
2025-11-13 20:06:03 -0300

    contador

--- AtividadeGustavo/js/script.js
+++ AtividadeGustavo/js/script.js
@@ -1,14 +1,20 @@
-function tocarSom(id) {
+function tocarSom() {
+    const contador = document.getElementById("contador");
+    let count = parseInt(contador.value);
+    count *= -1;
+
     //array de todos os audios da pagina para parar
-const audio = document.getElementById("audioCaneta");
+    const audio = document.getElementById("audioCaneta");
     const audios = document.querySelectorAll("audio");
 
     audios.forEach(a => {
         a.pause();
         a.currentTime = 0;
     });
-
-    if (audio) {
-        audio.play();
+    if (count === 1) {
+        if (audio) {
+            audio.play();
+        }
     }
+    contador.value = count;
 }
